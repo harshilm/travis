b4ab7ac1d05460f3de5f953742e70da8a9925f36
Harshil Maradiya <harshil.maradiya@gmail.com>
2019-02-14 14:58:19 +0530

    fixing python script

--- verify.py
+++ verify.py
@@ -1,1 +1,20 @@
-print("Hello how are you")
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.firefox.options import Options
+from time import sleep
+import requests, bs4
+
+target_url = 'https://www.pfizerpro.co.uk'
+options = Options()
+options.add_argument("--headless")
+browser = webdriver.Chrome(chrome_options=options, executable_path="C:\\HCP\\Travis\\travis\\chromedriver.exe")
+browser.get(target_url)
+sleep(1)
+try:
+    pageName = browser.execute_script("return (s.pageName);")
+    events = browser.execute_script("return (s.events);")
+    print(pageName) #optional for console validation
+    print(events) #optional for console validation
+except WebDriverException:
+    print("Exception on {}".format(url))
+browser.quit()
