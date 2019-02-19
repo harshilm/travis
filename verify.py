db55b794914ee3a51a8e35d36c713e53837bfb8a
Harshil Maradiya <harshil.maradiya@gmail.com>
2019-02-19 11:58:37 +0530

    Fixing

--- verify.py
+++ verify.py
@@ -7,6 +7,7 @@ import requests, bs4
 target_url = 'https://www.pfizerpro.co.uk'
 options = Options()
 options.add_argument("--headless")
+options.add_argument("--disable-dev-shm-usage")
 browser = webdriver.Chrome(chrome_options=options, executable_path="/home/travis/build/harshilm/travis/chromedriver")
 browser.get(target_url)
 sleep(1)
